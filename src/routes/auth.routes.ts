49094d98c05b06307fb0305a69eaa517ec191819
Micho-Tade <tadessetabuse@gmail.com>
2025-09-28 12:14:33 +0300

    fix 7

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -100,9 +100,7 @@ export default async function authRoutes(fastify: FastifyInstance) {
         grant_type: 'authorization_code',
         redirect_uri:
           process.env.GOOGLE_REDIRECT_URI ||
-          `${
-            process.env.SERVER_URL || 'http://localhost:4000'
-          }/api/auth/google/callback`,
+          'https://mym-nexus.onrender.com/api/auth/google/callback',
       })
       const tokenResponse = await fetch(tokenUrl, {
         method: 'POST',
