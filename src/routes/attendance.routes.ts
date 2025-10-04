5033f0a6e7f36079a19b888ab269050c7453a249
Micho-Tade <tadessetabuse@gmail.com>
2025-10-05 00:18:10 +0300

    checkpoint

--- src/routes/attendance.routes.ts
+++ src/routes/attendance.routes.ts
@@ -5,8 +5,11 @@ import {
   deleteAttendanceRecord,
   getAttendanceRecordById,
 } from '../controllers/attendance.controller'
+import { authenticate } from '../middleware/auth'
 
 async function attendanceRoutes(fastify: FastifyInstance) {
+  fastify.addHook('preHandler', authenticate)
+
   fastify.get('/', getAttendanceRecords)
   fastify.post('/', createAttendanceRecord)
   fastify.delete('/:id', deleteAttendanceRecord)
